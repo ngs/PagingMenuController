64c20f804234ce899ac047cd518f588602d81a70
Atsushi Nagase <a@ngs.io>
2015-11-27 04:36:15 +0900

    Make public setting currentPage

--- Pod/Classes/PagingMenuController.swift
+++ Pod/Classes/PagingMenuController.swift
@@ -18,8 +18,16 @@ final public class PagingMenuController: UIViewController, UIScrollViewDelegate 
     public weak var delegate: PagingMenuControllerDelegate?
     private var options: PagingMenuOptions!
     public private(set) var menuView: MenuView!
-    public private(set) var currentPage: Int = 0
-    public private(set) var currentViewController: UIViewController!
+    public var currentPage: Int = 0 {
+        didSet {
+            currentViewController = pagingViewControllers[currentPage]
+        }
+    }
+    public var currentViewController: UIViewController! {
+        didSet {
+            view.setNeedsLayout()
+        }
+    }
     public private(set) var visiblePagingViewControllers = [UIViewController]()
     public private(set) var pagingViewControllers = [UIViewController]() {
         willSet {
@@ -143,7 +151,6 @@ final public class PagingMenuController: UIViewController, UIScrollViewDelegate 
         layoutPagingViewControllers()
 
         currentPosition = currentPagingViewPosition()
-        currentViewController = pagingViewControllers[currentPage]
         moveToMenuPage(currentPage, animated: false)
     }
     
